0f03b3d78d4c5942deded8c0ea3268e4c60d70f2
Vimalkumar Velayudhan <vimal@disroot.org>
2020-07-12 08:05:00 +0100

    Enable site search with duckduckgo

--- src/js/script.js
+++ src/js/script.js
@@ -39,49 +39,34 @@ jQuery(function($) {
   });
 
   /* ==========================================================================
-   ghostHunter
+   Search
    ========================================================================== */
+  var searchField = $("#search-field");
+  $('.nav-search').on({
+    'click': function() {
+      html.addClass('search-active');
+      searchField.focus();
+      html.removeClass('menu-active');
+    }
+  });
 
-  if (typeof ghosthunter_key !== 'undefined') {
-    var searchField = $("#search-field");
-    $(".nav-search").css({
-      'display': 'block'
-    });
+  $('.search-close').on({
+    'click': function() {
+      html.removeClass('search-active');
+      searchField.val('');
+      $('#results').empty();
+    }
+  });
 
-    $('.nav-search').on({
-      'click': function() {
-        html.addClass('search-active');
-        searchField.focus();
-        html.removeClass('menu-active');
-      }
-    });
-
-    $('.search-close').on({
-      'click': function() {
+  $(document).keydown(function(e) {
+    if (e.key === "Escape") { // escape key maps to keycode `27`
+      if (html.hasClass('search-active')) {
         html.removeClass('search-active');
         searchField.val('');
         $('#results').empty();
       }
-    });
-
-    $(document).keydown(function(e) {
-      if (e.key === "Escape") { // escape key maps to keycode `27`
-        if (html.hasClass('search-active')) {
-          html.removeClass('search-active');
-          searchField.val('');
-          $('#results').empty();
-        }
-      }
-    });
-
-    searchField.ghostHunter({
-      results: "#results",
-      result_template: '<article class="post"><div class="inner"><div class="box post-box"><h2 class="post-title"><a href="{{link}}">{{title}}</a></h2><span class="post-meta">On <span class="post-date">{{pubDate}}</span></span></div></div></article>',
-      info_template: '',
-      displaySearchInfo: true,
-      includebodysearch: true
-    });
-  }
+    }
+  });
 
   /* ==========================================================================
    Parallax cover
